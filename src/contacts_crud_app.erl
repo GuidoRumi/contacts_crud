e6e1db4b9e278f6b451338a1acbcda020faf0fa4
Guido Rumi <guido.rumi@gmail.com>
2018-09-16 17:34:36 -0300

    Added leader_cron task to remove inactive users

--- src/contacts_crud_app.erl
+++ src/contacts_crud_app.erl
@@ -16,6 +16,7 @@
 
 start(_StartType, _StartArgs) ->
     ok = application:ensure_started(epgsql, permanent),
+    %% Compiles and loads cowboy routes
     Dispatch = cowboy_router:compile([
         {'_', [
             {"/contacts", contacts_crud_http_handler, [contacts]},
@@ -29,6 +30,12 @@ start(_StartType, _StartArgs) ->
         [{port, 8080}],
         #{env => #{dispatch => Dispatch}}
     ),
+    %% Starts leader_cron task to delete inactive users
+    %% every 10 minutes
+    leader_cron:start_link([node()]),
+    leader_cron:schedule_task(
+        {cron, {[0, 10, 20, 30, 40, 50], all, all, all, all}},
+        {contacts_crud_queries, delete_inactive, []}),
     contacts_crud_sup:start_link().
 
 %%--------------------------------------------------------------------
